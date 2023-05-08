e94aa36cc5fd09fed9f3866bf925dcf4ae4a1a56
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-05-08 12:55:53 +0300

    get user data

---
+++ Task-1/index.js
@@ -0,0 +1,18 @@
+export const fetchUser = async (userId) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    console.log(response);
+    if (!response.ok) {
+      return null;
+    }
+
+    const userData = await response.json();
+    return userData;
+  } catch (err) {
+    throw new Error('Failed to get user data');
+  }
+};
+
+fetchUser('facebook')
+  .then((userData) => console.log(userData))
+  .catch((err) => alert(err.message));
