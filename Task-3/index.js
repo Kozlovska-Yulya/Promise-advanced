2fcabd734b8ee2b5ce125532a5719d352d9c62cb
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-05-11 12:43:57 +0300

    promise all users

--- Task-3/index.js
+++ Task-3/index.js
@@ -1,18 +1,18 @@
 export const getUsersBlogs = async (users) => {
   try {
     const requests = users
-      .map((user) => fetch(`https://api.github.com/users/${user}`))
+      .map((userId) => fetch(`https://api.github.com/users/${userId}`))
       .then((response) => response.json());
 
     const userData = await Promise.all(requests);
     return userData;
   } catch (err) {
-    console.log(err);
+    throw new Error('Failed to load data');
   }
 };
 
-// examples
-getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
-  console.log(linksList)
-); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+// // examples
+// getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
+//   console.log(linksList)
+// ); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+// getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
