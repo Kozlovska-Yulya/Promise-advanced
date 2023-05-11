3362d6166b383725049d6d9fee30ec17bd992d05
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-05-11 22:36:04 +0300

    Promise all users

--- Task-3/index.js
+++ Task-3/index.js
@@ -1,18 +1,19 @@
-export const getUsersBlogs = async (users) => {
+export const getUsersBlogs = async (usersList) => {
   try {
-    const requests = users
-      .map((userId) => fetch(`https://api.github.com/users/${userId}`))
-      .then((response) => response.json());
-
-    const userData = await Promise.all(requests);
-    return userData;
+    const requests = usersList.map((userId) =>
+      fetch(`https://api.github.com/users/${userId}`).then((response) =>
+        response.json()
+      )
+    );
+    const usersData = await Promise.all(requests);
+    return usersData.map(({ blog }) => blog);
   } catch (err) {
     throw new Error('Failed to load data');
   }
 };
 
-// // examples
-// getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
-//   console.log(linksList)
-// ); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-// getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+// examples
+getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
+  console.log(linksList)
+); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
